79fa7f77279e2e97e7227adac3fed3020fd9fc57
rvvichitra-sys <rvvichitra@gmail.com>
2026-05-26 15:03:45 +0530

    Allow all CORS origins for deployed frontend access

--- backend/main.py
+++ backend/main.py
@@ -22,8 +22,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
